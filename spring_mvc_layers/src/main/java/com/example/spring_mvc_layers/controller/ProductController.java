8b00d8ddeb14c222e81918d01475e13726b0645d
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-13 15:21:26 +0530

    Send Response to user

--- spring_mvc_layers/src/main/java/com/example/spring_mvc_layers/controller/ProductController.java
+++ spring_mvc_layers/src/main/java/com/example/spring_mvc_layers/controller/ProductController.java
@@ -2,15 +2,20 @@ package com.example.spring_mvc_layers.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring_mvc_layers.model.Product;
+import com.example.spring_mvc_layers.service.ProductService;
 
 @RestController
 public class ProductController {
 
+	@Autowired
+	ProductService service;
+	
 	@RequestMapping("/")
 	public String home() {
 		return "This is Home Page of Our Project";
@@ -18,6 +23,6 @@ public class ProductController {
 	
 	@GetMapping("/products")
 	public List<Product> getAllProducts(){
-		return null;
+		return service.getAllProducts();
 	}
 }
