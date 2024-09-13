86b735c26bc3075b5c678da8d1222292270a6835
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-13 14:53:41 +0530

    Initial Commit

---
+++ spring_mvc_layers/src/main/java/com/example/spring_mvc_layers/SpringMvcLayersApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_layers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLayersApplication.class, args);
+	}
+
+}
